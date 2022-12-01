dea5556eaa147ff1ad8fb3fdc6d1a69bfb90daa0
Carlos Oliveira <carlos.oliveira@wundermanthompson.com>
2022-12-01 18:12:38 +0000

    Fix bugs

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 import fs from 'fs';
-import { puzzle_1a, puzzle_1b } from './lib/puzzle_1/index.js';
+import { puzzle_1a, puzzle_1b, validate_1a, validate_1b } from './lib/puzzle_1/index.js';
 
 const puzzles = {
   "1a": puzzle_1a,
   "1b": puzzle_1b
 };
 
+const validate = {
+  "1a": validate_1a,
+  "1b": validate_1b
+};
+
 const puzzleName= process.argv[2];
 const index = parseInt(puzzleName);
 const dataName = process.argv[3] === 'test' ? 'test' : 'final';
+const filename = `./data/puzzle_${index}/${dataName}.txt`;
 
-const file = fs.readFileSync(`./data/puzzle_${index}/${dataName}.txt`, 'ascii');
+console.log(filename)
 
-puzzles[puzzleName](file);
+const file = fs.readFileSync(filename, 'ascii');
+
+const answer = puzzles[puzzleName](file);
+
+if (dataName === 'test') {
+  const valid = validate[puzzleName](answer);
+
+  console.log(`Answer for puzzle ${puzzleName}: ${answer} (${valid ? 'Correct' : 'Wrong'})`);
+} else {
+  console.log(`Answer for puzzle ${puzzleName}: ${answer}`);
+}
