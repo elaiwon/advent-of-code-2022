c5c512f34a63068c72b33f0aa2a594a26d5b6b70
Carlos Oliveira <carlos.oliveira@wundermanthompson.com>
2022-12-01 11:48:34 +0000

    Add a simple command line option

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import { puzzle_1b } from './lib/puzzle_1/index.js';
+import fs from 'fs';
+import { puzzle_1a, puzzle_1b } from './lib/puzzle_1/index.js';
 
-puzzle_1b();
+const puzzles = {
+  "1a": puzzle_1a,
+  "1b": puzzle_1b
+};
+
+const puzzleName= process.argv[2];
+const index = parseInt(puzzleName);
+const dataName = process.argv[3] === 'test' ? 'test' : 'final';
+
+const file = fs.readFileSync(`./data/puzzle_${index}/${dataName}.txt`, 'ascii');
+
+puzzles[puzzleName](file);
