64fbc11f72de7c54f771fd0d945018022f77bd35
Carlos Oliveira <carlos.oliveira@wundermanthompson.com>
2022-12-02 11:15:22 +0000

    Puzzle 2

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 import fs from 'fs';
 import { puzzle_1a, puzzle_1b, validate_1a, validate_1b } from './lib/puzzle_1/index.js';
+import { puzzle_2a, puzzle_2b, validate_2a, validate_2b } from './lib/puzzle_2/index.js';
 
 const puzzles = {
   "1a": puzzle_1a,
-  "1b": puzzle_1b
+  "1b": puzzle_1b,
+  "2a": puzzle_2a,
+  "2b": puzzle_2b
 };
 
 const validate = {
   "1a": validate_1a,
-  "1b": validate_1b
+  "1b": validate_1b,
+  "2a": validate_2a,
+  "2b": validate_2b
 };
 
 const puzzleName= process.argv[2];
 const index = parseInt(puzzleName);
 const dataName = process.argv[3] === 'test' ? 'test' : 'final';
 const filename = `./data/puzzle_${index}/${dataName}.txt`;
-
-console.log(filename)
 
 const file = fs.readFileSync(filename, 'ascii');
 
