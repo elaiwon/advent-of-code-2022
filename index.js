f037f81fd36331406f807a0ee787a192ff755426
Carlos Oliveira <carlos.oliveira@wundermanthompson.com>
2022-12-04 15:58:45 +0000

    Puzzle 4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import { puzzle_1a, puzzle_1b, validate_1a, validate_1b } from './lib/puzzle_1/index.js';
 import { puzzle_2a, puzzle_2b, validate_2a, validate_2b } from './lib/puzzle_2/index.js';
 import { puzzle_3a, puzzle_3b, validate_3a, validate_3b } from './lib/puzzle_3/index.js';
+import { puzzle_4a, puzzle_4b, validate_4a, validate_4b } from './lib/puzzle_4/index.js';
 
 const puzzles = {
   "1a": puzzle_1a,
@@ -9,7 +10,9 @@ const puzzles = {
   "2a": puzzle_2a,
   "2b": puzzle_2b,
   "3a": puzzle_3a,
-  "3b": puzzle_3b
+  "3b": puzzle_3b,
+  "4a": puzzle_4a,
+  "4b": puzzle_4b
 };
 
 const validate = {
@@ -18,7 +21,9 @@ const validate = {
   "2a": validate_2a,
   "2b": validate_2b,
   "3a": validate_3a,
-  "3b": validate_3b
+  "3b": validate_3b,
+  "4a": validate_4a,
+  "4b": validate_4b
 };
 
 const puzzleName= process.argv[2];
